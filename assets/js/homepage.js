1d592cccf5bd620112fe644ef515060ab6d78073
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2022-08-27 13:22:33 -0400

    github api can now fetch specific user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,30 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+// var getUserRepos = function() {
+//     fetch("https://api.github.com/users/octocat/repos");
+
+//     var response = fetch("https://api.github.com/users/octocat/repos");
+//     console.log(response);
+// };
+
+// var getUserRepos = function() {
+//     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//         response.json().then(function(data){
+//             console.log(data);
+//         });
+//     });
+
+//     console.log("outside");
+// }
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + "0xZ3n" + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
