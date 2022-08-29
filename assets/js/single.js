143ec094318c4c28450e62e0f9922e8b53e8877e
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2022-08-29 13:18:06 -0400

    now display warnings for request problems, message and link for accessing repos over list of 30

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -11,12 +11,21 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 //pass response data to dom function
                 displayIssues(data);
+
+                 //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+                 
+                
             });
         }
         else {
             alert("There was a problem with your request!");
         }
     });
+
+
 };
 
 
@@ -59,6 +68,19 @@ var displayIssues = function(issues) {
 
     
 
+};
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 
