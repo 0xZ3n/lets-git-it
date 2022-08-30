6bd61f4f4c23026496ab01531a5e0e0f7d2a4dff
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2022-08-29 22:04:07 -0400

    can get repo by name, return to homepage if none exists

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,28 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+//var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+
+
+//get repo name in api call
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+};
+
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -21,7 +44,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 
@@ -84,4 +108,5 @@ var displayWarning = function(repo) {
 };
 
 
-getRepoIssues("facebook/react");
+//getRepoIssues();
+getRepoName();
